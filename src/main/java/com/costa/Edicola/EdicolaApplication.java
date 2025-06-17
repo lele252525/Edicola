09ee9d1a6646d57845befea3fb3d49786dc97524
Daniele Costa <costadaniele2016@gmail.com>
2025-06-17 15:17:41 +0200

    Primo Commit

---
+++ src/main/java/com/costa/Edicola/EdicolaApplication.java
@@ -0,0 +1,13 @@
+package com.costa.Edicola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdicolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdicolaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/costa/Edicola/EdicolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.costa.Edicola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdicolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
