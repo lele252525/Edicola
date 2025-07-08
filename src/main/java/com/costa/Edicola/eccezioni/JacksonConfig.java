3c4db3c335473d8ddf201707e39f01de885c27f1
Daniele Costa <costadaniele2016@gmail.com>
2025-07-08 12:22:00 +0200

    Versione 1.5

--- src/main/java/com/costa/Edicola/eccezioni/JacksonConfig.java
+++ src/main/java/com/costa/Edicola/eccezioni/JacksonConfig.java
@@ -1,5 +1,27 @@
 package com.costa.Edicola.eccezioni;
 
-public class JacksonConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+@Configuration
+public class JacksonConfig {
+	
+	@Bean
+    public JavaTimeModule javaTimeModule() {
+        return new JavaTimeModule();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.setDateFormat(new java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss"));
+        return objectMapper;
+    }
+	
 }
