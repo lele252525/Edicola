f15f1b248cf9b506b8d6bd1b7fb66657a54c9647
Daniele Costa <costadaniele2016@gmail.com>
2025-07-26 09:09:09 +0200

    Versione 1.8

--- src/main/java/com/costa/Edicola/costanti/EdicolaCostanti.java
+++ src/main/java/com/costa/Edicola/costanti/EdicolaCostanti.java
@@ -2,7 +2,7 @@ package com.costa.Edicola.costanti;
 
 public class EdicolaCostanti {
 	
-	public static final String INGRESSO_END_POINT = "/gestione_biglietti";
+	public static final String INGRESSO_END_POINT = "/gestione_edicola";
 	public static final String NUOVO_BIGL_END_POINT = "/crea_biglietto";
 	public static final String LEGGI_BIGL_END_POINT = "/leggi_biglietto";
 	public static final String CANC_BIGL_END_POINT = "/cancella_biglietto";
